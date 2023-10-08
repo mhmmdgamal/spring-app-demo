87092a3c62f0bcc147fd8d7ac8121872e5f13665
Mohamed Gamal <mohamed.gamal987.mg@gmail.com>
2023-10-08 17:42:26 +0300

    migrate to spring boot 3

--- src/main/java/com/study/demo/DemoApplication.java
+++ src/main/java/com/study/demo/DemoApplication.java
@@ -2,12 +2,21 @@ package com.study.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/test")
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@GetMapping
+	public String test() {
+		return "test";
+	}
 }
